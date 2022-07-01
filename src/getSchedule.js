7d5cc203a3a351a946108a5b76611687d536e058
Guilherme Garcia <garciaguig@gmail.com>
2022-06-30 22:58:45 -0300

    Conclusão do Requisito 09 - Implemente a função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { hours, species } = data;
+const animalsList = species.map((specie) => specie.name);
+const daysList = Object.keys(hours);
+
+function generateDaySchedule(day) {
+  if (day === 'Monday') {
+    const officeHour = 'CLOSED';
+    const exhibition = 'The zoo will be closed!';
+    return { officeHour, exhibition };
+  }
+  const officeHour = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  const exhibition = species
+    .filter((specie) => specie.availability.includes(day))
+    .map((specie) => specie.name);
+  return { officeHour, exhibition };
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const schedule = {};
+  if (animalsList.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  if (daysList.includes(scheduleTarget)) {
+    schedule[scheduleTarget] = generateDaySchedule(scheduleTarget);
+    return schedule;
+  }
+  daysList.forEach((day) => {
+    schedule[day] = generateDaySchedule(day);
+  });
+  return schedule;
 }
 
 module.exports = getSchedule;

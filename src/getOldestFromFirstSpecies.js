2b13b0e2258886fdd1cd5c58f8bd10690023aaf4
Guilherme Garcia <garciaguig@gmail.com>
2022-07-01 00:01:39 -0300

    Conclusão do Requisito 10 - Implemente a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+const defineOldest = (oldest, current) => ((oldest > current.age) ? oldest : current.age);
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const collaborator = employees.find((employee) => employee.id === id);
+  const firstAnimal = collaborator.responsibleFor
+    .map((animal) => species
+      .find((specie) => specie.id === animal))[0];
+  const firstResidents = firstAnimal.residents;
+  const firstOldest = firstResidents
+    .find((resident) => resident.age === firstResidents
+      .reduce(defineOldest, firstResidents[0].age));
+  return Object.values(firstOldest);
 }
 
 module.exports = getOldestFromFirstSpecies;

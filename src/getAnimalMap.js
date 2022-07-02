0b391e289d413a00a9ce74c564cb5b65ddf5f868
Guilherme Garcia <garciaguig@gmail.com>
2022-07-02 20:35:06 -0300

    Conclusão do Requisito 14 - Implemente a função getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+function getOnlySpeciesNames() {
+  const obj = {};
+  locations.forEach((location) => {
+    const animals = species.filter((specie) => specie.location === location)
+      .map((specie) => specie.name);
+    obj[location] = animals;
+  });
+  return obj;
+}
+
+function getAllSpeciesInfo(sortInfo, sexInfo) {
+  const array = [];
+  locations.forEach((location) => {
+    const animals = species.filter((specie) => specie.location === location)
+      .map((specie) => {
+        const obj = {};
+        let info = specie.residents.map((resident) => resident);
+        if (sexInfo) info = info.filter((resident) => resident.sex === sexInfo);
+        info = info.map((resident) => resident.name);
+        if (sortInfo) info.sort();
+        obj[specie.name] = info;
+        return obj;
+      });
+    array.push(animals);
+  });
+  return array;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) return getOnlySpeciesNames();
+  const { includeNames, sex, sorted } = options;
+  if (includeNames) {
+    const animals = getAllSpeciesInfo(sorted, sex);
+    const obj = {};
+    locations.forEach((location, index) => {
+      obj[location] = animals[index];
+    });
+    return obj;
+  }
+  return getOnlySpeciesNames();
 }
 
 module.exports = getAnimalMap;

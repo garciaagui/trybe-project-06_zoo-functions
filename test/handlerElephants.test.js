0bd3f78e97ab989fda02cbefc97c997476e5f9dc
Guilherme Garcia <garciaguig@gmail.com>
2022-07-04 14:57:20 -0300

    Conclusão do Requisito 15 - Obtenha 100% de cobertura de testes na função handlerElephants [Correção - Concluído junto aos Requisitos 06 e 07]

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,5 +1,5 @@
 const handlerElephants = require('../src/handlerElephants');
-
+//
 describe('Testes da função HandlerElephants', () => {
   it('Testa se é uma função', () => {
     expect(typeof handlerElephants).toBe('function');
